8b0c4badb5075f5f3747f1734c3e108daba14ce0
Your Name <oscar.rodriguez@koombea.com>
2011-02-14 13:31:26 -0500

    adding folder controller

---
+++ controllers/drink_controller.rb
@@ -0,0 +1,25 @@
+get '/Drink/list' do
+  unless requires_authorization!
+  @drinks = Drink.all
+  @drinks.to_json
+  end
+end
+
+
+post '/Drink/new' do
+  unless requires_authorization!
+    if params['name']!=""&&params['description']!=""
+      drink = Drink.new
+      drink.name=params[:name]
+      drink.description=params[:description]
+      drink.picture_url=params[:picture_url]
+      if drink.save
+       {"response"=>{"status"=>"true","error_code"=>"0"}}.to_json
+      else
+        {"response"=>{"status"=>"true","error_code"=>"104"}}.to_json
+      end
+    else
+      {"response"=>{"status"=>"true","error_code"=>"100"}}.to_json
+    end
+  end
+end
